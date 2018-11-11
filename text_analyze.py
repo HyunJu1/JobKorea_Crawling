a9769152d26f8b1d0fca7525046faa342984e12e
Hyunju1 <hh960305@naver.com>
2018-11-11 15:10:31 +0900

    data processing

--- text_analyze.py
+++ text_analyze.py
@@ -8,12 +8,17 @@ rc('font', family=font_name)
 
 
 df=pd.read_excel('sample1.xlsx',header=0)
-df[['합격자 토익','합격자 수상횟수','지원자 수']] = df[['합격자 토익','합격자 수상횟수','지원자 수']].apply(pd.to_numeric) 
+#df[['합격자 토익','합격자 수상횟수','지원자 수']] = df[['합격자 토익','합격자 수상횟수','지원자 수']].apply(pd.to_numeric, errors='coerce') 
+df['회사 직원 수']=(pd.to_numeric(df['회사 직원 수'].replace(',',''), errors='coerce'))
+df['평균 연봉']=(pd.to_numeric(df['평균 연봉'].replace(',',''), errors='coerce'))
+
+if df['회사 설립년도'].any()=="년차)":
+	df['회사 설립 년도']=df['회사 직원 수']
+	df['회사 직원 수']=''
+
+df['회사 설립년도']=(pd.to_numeric(df['회사 설립년도'].replace(',',''), errors='coerce'))
+
 print(df.info())
 print(df.head())
 
-
-#plt.plot(df['합격자 수상횟수'],df['지원자 수'])
-
-#plt.show()
-df.to_excel("sample3.xlsx")
+df.to_excel("sample5.xlsx")
