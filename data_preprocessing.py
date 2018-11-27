5bc6135ad5ae842f99ff844b1e4be9c9d4707be1
Hyunju1 <hh960305@naver.com>
2018-11-27 15:59:05 +0900

    edit preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,23 +3,28 @@ import pandas as pd
 import matplotlib.font_manager as fm
 from matplotlib import rc
 from matplotlib import pyplot as plt
+import re
 font_name = fm.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 rc('font', family=font_name)
 
 
 df=pd.read_excel('sample_5200.xlsx',header=0)
 
-df['회사 직원 수']=(pd.to_numeric(df['회사 직원 수'].replace(',',''), errors='coerce'))
-df['평균 연봉']=(pd.to_numeric(df['평균 연봉'].replace(',',''), errors='coerce'))
+df['comp_member_number']=(pd.to_numeric(df['comp_member_number'].str.replace(',',''), errors='coerce'))
+df['avg_salary']=(pd.to_numeric(df['avg_salary'].str.replace(',',''), errors='coerce'))
 
-if df['회사 설립년도'].any()=="년차)":
-	df['회사 설립 년도']=df['회사 직원 수']
-	df['회사 직원 수']=''
+#df = df.apply(lambda x: x.str.replace(',', ' '))
+if df['comp_year'].any()=="년차)":
+	df['comp_year']=df['comp_member_number']
+	df['comp_member_number']=''
 
-df['회사 설립년도']=(pd.to_numeric(df['회사 설립년도'].replace(',',''), errors='coerce'))
-
+df['comp_year']=(pd.to_numeric(df['comp_year'].replace(',',''), errors='coerce'))
+df['comp_spec']=df['comp_spec'].str.replace(',','-')
+df['comp_level']=df['comp_level'].str.replace('\n','').replace('\t','')
 #df['회사 영업이익']=(pd.to_numeric(df['회사 영업이익'].replace(',','').replace('억','').replace('천','000').replace('십','0'), errors='coerce'))
+
+
 print(df.info())
 print(df.head())
 
-df.to_excel("test.xlsx")
+df.to_excel("result_5200_test.xlsx",encoding="utf-8")
